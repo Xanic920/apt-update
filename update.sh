b0c3e9968ebd28a26048f94208075a6c90d4ece2
Xanic <53575336+Xanic920@users.noreply.github.com>
2024-09-20 16:29:10 +0200

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,7 +4,13 @@
 apt update && apt install apt-transport-https ca-certificates sudo curl -y
 
 # Replace http with https in all .list files in /etc/apt, except for a specific line
-sudo find /etc/apt -type f -name "*.list" -exec sed -i '/^deb http:\/\/download.proxmox.com\/debian\/pve bookworm/!s|http://|https://|g' {} +
-
+sudo find /etc/apt -type f -name "*.list" -exec sed -i '
+# Proxmox Repository Anpassung für Bookworm
+/^deb http:\/\/download.proxmox.com\/debian\/pve bookworm/!s|http://|https://|g
+# Proxmox Repository Anpassung für Bullseye
+/^deb http:\/\/download.proxmox.com\/debian\/pve bullseye/!s|http://|https://|g
+# Proxmox Repository Anpassung für Buster
+/^deb http:\/\/download.proxmox.com\/debian\/pve buster/!s|http://|https://|g
+' {} +
 # Update, upgrade, and remove unnecessary packages
 apt update && apt dist-upgrade -y && apt autoremove -y
